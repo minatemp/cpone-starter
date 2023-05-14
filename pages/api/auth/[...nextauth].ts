c1c11037cc7518275e58a968a9728ed998983691
minatemp <sumberita@outlook.com>
2023-05-14 12:36:29 +0300

    Commit 2

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,5 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import TwitterProvider from "next-auth/providers/twitter"
-import GithubProvider from "next-auth/providers/github"
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -8,10 +7,6 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.TWITTER_ID,
       clientSecret: process.env.TWITTER_SECRET,
       version: "2.0",
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
   theme: {
